82f8c952053aea9ee81b7e331f3b1675f1c0d541
stimm <stimm@outlook.com>
2020-04-18 12:19:58 +0200

    Removed Settings except spoiler settings, FC is always activated after getting final achievement of main campaign

--- GloomhavenCampaignTracker.Droid/Business/Settings.cs
+++ GloomhavenCampaignTracker.Droid/Business/Settings.cs
@@ -64,7 +64,7 @@ namespace GloomhavenCampaignTracker.Droid
 
         public bool IsFCActivated
         {
-            get => isFCActivated;
+            get => true;// isFCActivated;
             set
             {
                 isFCActivated = value;
